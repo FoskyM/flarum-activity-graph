4c92cbea0563e6996b83957eb3ad77102f072380
FoskyM <i@fosky.top>
2024-06-27 21:25:02 +0800

    feat: add permission of querying others' activity graph

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,5 +1,11 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('foskym/flarum-activity-graph', () => {
-  console.log('[foskym/flarum-activity-graph] Hello, admin!');
+  app.extensionData.for('foskym-activity-graph')
+    .registerPermission({
+        icon: 'fas fa-id-card',
+        label: app.translator.trans('foskym-activity-graph.admin.settings.query-others-activity-graph'),
+        permission: 'foskym-activity-graph.queryOthersActivityGraph',
+        allowGuest: true
+      }, 'view')
 });
