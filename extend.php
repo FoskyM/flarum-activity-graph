20f86edfbf3dbdff58350c0447823d03977f939d
FoskyM <i@fosky.top>
2024-06-27 21:18:03 +0800

    pref: echarts loading

--- extend.php
+++ extend.php
@@ -20,7 +20,10 @@ return [
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less')
         ->content(function (Document $document, Request $request) {
-            $document->head[] = '<script src="https://cdn.jsdelivr.net/npm/echarts@5.5.0/dist/echarts.min.js"></script>';
+            $requestUri = $request->getUri()->getPath();
+            if (strpos($requestUri, '/activity-graph') !== false) {
+                $document->head[] = '<script src="https://fastly.jsdelivr.net/npm/echarts@5.5.0/dist/echarts.min.js"></script>';
+            }
         }),
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
